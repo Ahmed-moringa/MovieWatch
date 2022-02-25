fd8fc69a6507abed833fd3236721d6c2b1802853
Ahmed-moringa <ahmed.mohamed@student.moringaschool.com>
2022-02-25 09:18:29 +0300

    added cloudinary storage

--- mysite/settings.py
+++ mysite/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 from decouple import config,Csv
 import dj_database_url
 import django_heroku
+import cloudinary_storage
 
 MODE=config("MODE", default="dev")
 
@@ -46,7 +47,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'cloudinary_storage'
     'django.contrib.staticfiles',
+    'cloudinary',
 ]
 
 MIDDLEWARE = [
@@ -144,7 +147,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
+STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 STATIC_DIR = os.path.join(BASE_DIR,'static')
@@ -153,7 +156,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATICFILES_DIRS = [
     STATIC_DIR,
 ]
-
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
